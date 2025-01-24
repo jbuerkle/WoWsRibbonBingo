4d0c85436d7318678e880d790ccc1074494966ed
jbuerkle <joel.buerkle@web.de>
2025-01-24 20:26:53 +0100

    Write tests for RibbonResult class

---
+++ test/ribbons/RibbonResultTest.java
@@ -0,0 +1,45 @@
+package ribbons;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class RibbonResultTest {
+
+    @Test
+    void getPointValueShouldReturnCorrectValue() {
+        RibbonResult resultA = new RibbonResult(Ribbon.SET_ON_FIRE, 11);
+        RibbonResult resultB = new RibbonResult(Ribbon.CAUSED_FLOODING, 3);
+        RibbonResult resultC = new RibbonResult(Ribbon.SHELL_HIT, 30);
+        RibbonResult resultD = new RibbonResult(Ribbon.SECONDARY_HIT, 110);
+        assertEquals(220, resultA.getPointValue());
+        assertEquals(120, resultB.getPointValue());
+        assertEquals(30, resultC.getPointValue());
+        assertEquals(110, resultD.getPointValue());
+    }
+
+    @Test
+    void toStringMethodShouldReturnCorrectDisplayText() {
+        RibbonResult resultA = new RibbonResult(Ribbon.SET_ON_FIRE, 11);
+        RibbonResult resultB = new RibbonResult(Ribbon.CAUSED_FLOODING, 3);
+        assertEquals("Set on fire: 11 * 20 points", resultA.toString());
+        assertEquals("Caused flooding: 3 * 40 points", resultB.toString());
+    }
+
+    @Test
+    void resultShouldBeEqualWithSameRibbon() {
+        RibbonResult resultA = new RibbonResult(Ribbon.SHELL_HIT, 30);
+        RibbonResult resultB = new RibbonResult(Ribbon.SHELL_HIT, 110);
+        assertEquals(resultA, resultB);
+        assertEquals(resultA.hashCode(), resultB.hashCode());
+    }
+
+    @Test
+    void resultShouldNotBeEqualWithDifferentRibbon() {
+        RibbonResult resultA = new RibbonResult(Ribbon.SHELL_HIT, 50);
+        RibbonResult resultB = new RibbonResult(Ribbon.SECONDARY_HIT, 50);
+        assertNotEquals(resultA, resultB);
+        assertNotEquals(resultA.hashCode(), resultB.hashCode());
+    }
+}
