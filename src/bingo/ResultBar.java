668146233349cff9bba8e9f415bf440b20d18d4a
jbuerkle <joel.buerkle@web.de>
2025-01-25 04:03:39 +0100

    Create ResultBar class

---
+++ src/bingo/ResultBar.java
@@ -0,0 +1,33 @@
+package bingo;
+
+public class ResultBar {
+    private final int level;
+
+    public ResultBar(int level) {
+        this.level = level;
+    }
+
+    public int getPointRequirement() {
+        return getPointRequirement(this.level);
+    }
+
+    private int getPointRequirement(int currentLevel) {
+        final int pointRequirementForCurrentLevel;
+        if (currentLevel > 6) {
+            pointRequirementForCurrentLevel = 50;
+        } else if (currentLevel > 4) {
+            pointRequirementForCurrentLevel = 100;
+        } else if (currentLevel > 2) {
+            pointRequirementForCurrentLevel = 150;
+        } else if (currentLevel > 0) {
+            pointRequirementForCurrentLevel = 200;
+        } else {
+            return 0;
+        }
+        return pointRequirementForCurrentLevel + getPointRequirement(currentLevel - 1);
+    }
+
+    public int getNumberOfSubsAsReward() {
+        return (int) Math.pow(2, this.level - 1);
+    }
+}
