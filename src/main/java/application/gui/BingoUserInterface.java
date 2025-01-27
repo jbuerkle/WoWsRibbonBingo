a8bca6ea3c7a10cf8d19782de7207eddb417d70a
jbuerkle <joel.buerkle@web.de>
2025-01-27 03:13:08 +0100

    Write code to generate all UI input fields

--- src/main/java/application/gui/BingoUserInterface.java
+++ src/main/java/application/gui/BingoUserInterface.java
@@ -1,23 +1,67 @@
 package application.gui;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import ribbons.Ribbon;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class BingoUserInterface extends Application {
+    private final Map<Ribbon, TextField> textFieldsByRibbon;
+    private final GridPane mainGrid;
+    private int mainGridRow;
+
+    public BingoUserInterface() {
+        textFieldsByRibbon = new HashMap<>();
+        mainGrid = new GridPane();
+        mainGridRow = 0;
+        setUpGridWithFourInputFieldsPerRow();
+    }
+
+    private void setUpGridWithFourInputFieldsPerRow() {
+        GridPane gridPane = createNewGridPane();
+        int column = 0;
+        for (Ribbon ribbon : Ribbon.values()) {
+            if (column > 3) {
+                mainGridRow++;
+                gridPane = createNewGridPane();
+                column = 0;
+            }
+            createInputFieldForRibbon(ribbon, gridPane, column);
+            column++;
+        }
+        mainGridRow++;
+    }
+
+    private GridPane createNewGridPane() {
+        GridPane gridPane = new GridPane();
+        gridPane.setVgap(10);
+        gridPane.setHgap(40);
+        gridPane.setPadding(new Insets(15));
+        mainGrid.add(gridPane, 0, mainGridRow);
+        return gridPane;
+    }
+
+    private void createInputFieldForRibbon(Ribbon ribbon, GridPane gridPane, int column) {
+        Label label = new Label(ribbon.getDisplayText());
+        TextField textField = new TextField();
+        gridPane.add(label, column, 0);
+        gridPane.add(textField, column, 1);
+        gridPane.getColumnConstraints().add(new ColumnConstraints());
+        textFieldsByRibbon.put(ribbon, textField);
+    }
 
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button("Click Me");
-        btn.setOnAction(e -> System.out.println("Hello JavaFX!"));
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(root, 300, 250);
-        primaryStage.setTitle("JavaFX Demo");
+        Scene scene = new Scene(mainGrid);
+        primaryStage.setTitle("World of Warships Ribbon Bingo");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
