d547ad0025b6f288aca5044057c58e1f09286bd5
jbuerkle <joel.buerkle@web.de>
2025-01-28 02:47:39 +0100

    Write test for new method in ResultBar class

--- src/test/java/bingo/ResultBarTest.java
+++ src/test/java/bingo/ResultBarTest.java
@@ -1,6 +1,7 @@
 package bingo;
 
 import org.junit.jupiter.api.Test;
+import ribbons.Ribbon;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -36,5 +37,12 @@ class ResultBarTest {
         assertEquals(32, new ResultBar(6).getNumberOfSubsAsReward());
         assertEquals(64, new ResultBar(7).getNumberOfSubsAsReward());
         assertEquals(128, new ResultBar(8).getNumberOfSubsAsReward());
+    }
+
+    @Test
+    void getNumberOfSubsAsStringShouldReturnCorrectDisplayText() {
+        assertEquals("1 sub", new ResultBar(1).getNumberOfSubsAsString());
+        assertEquals("2 subs", new ResultBar(2).getNumberOfSubsAsString());
+        assertEquals("4 subs", new ResultBar(3).getNumberOfSubsAsString());
     }
 }
