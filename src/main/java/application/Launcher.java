6680e457259f39057d6e2ec529c07832ee3042f7
jbuerkle <joel.buerkle@web.de>
2025-01-30 05:56:46 +0100

    Define separate Launcher class with main method

---
+++ src/main/java/application/Launcher.java
@@ -0,0 +1,9 @@
+package application;
+
+import application.gui.BingoUserInterface;
+
+public class Launcher {
+    public static void main(String[] args) {
+        BingoUserInterface.main(args);
+    }
+}
