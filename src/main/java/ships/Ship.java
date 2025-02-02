dd3c4618483b71b8e296ec07677c3b8a4a3ab892
jbuerkle <joel.buerkle@web.de>
2025-02-02 23:54:39 +0100

    Create very basic record for ships

---
+++ src/main/java/ships/Ship.java
@@ -0,0 +1,13 @@
+package ships;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public record Ship(String name) {
+
+    public StringProperty nameProperty() {
+        StringProperty shipNameProperty = new SimpleStringProperty(this, "name");
+        shipNameProperty.setValue(name);
+        return shipNameProperty;
+    }
+}
---
+++ src/test/java/ships/ShipTest.java
@@ -0,0 +1,17 @@
+package ships;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ShipTest {
+    private static final String DUMMY_SHIP = "Dummy Ship";
+
+    @Test
+    void getterMethodsShouldReturnName() {
+        Ship ship = new Ship(DUMMY_SHIP);
+        assertEquals(DUMMY_SHIP, ship.name());
+        assertEquals(DUMMY_SHIP, ship.nameProperty().get());
+        assertEquals("name", ship.nameProperty().getName());
+    }
+}
