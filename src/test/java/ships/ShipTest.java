7b18f276395039e0022866628261675459d58899
jbuerkle <joel.buerkle@web.de>
2025-02-05 00:53:22 +0100

    Make test class package-scoped (just like the others)

--- src/test/java/ships/ShipTest.java
+++ src/test/java/ships/ShipTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ShipTest {
+class ShipTest {
     private static final String DUMMY_SHIP = "Dummy Ship";
 
     @Test
