ed2410723d06a35ea50518b3fc2d3b819850cfd5
jbuerkle <joel.buerkle@web.de>
2025-03-09 05:05:44 +0100

    Define new enums for bingo game states and actions

---
+++ src/main/java/bingo/game/BingoGameAction.java
@@ -0,0 +1,8 @@
+package bingo.game;
+
+public enum BingoGameAction {
+    SUBMIT_RESULT,
+    CONFIRM_RESULT,
+    RESET_WITHOUT_CONFIRMING,
+    END_CHALLENGE_VOLUNTARILY
+}
---
+++ src/main/java/bingo/game/BingoGameState.java
@@ -0,0 +1,8 @@
+package bingo.game;
+
+public enum BingoGameState {
+    LEVEL_INITIALIZED,
+    UNCONFIRMED_RESULT,
+    UNCONFIRMED_VOLUNTARY_END,
+    CHALLENGE_ENDED
+}
