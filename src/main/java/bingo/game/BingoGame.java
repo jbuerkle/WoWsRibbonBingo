e9bdf56cf2917aef29633676e5a73f734cacae21
jbuerkle <joel.buerkle@web.de>
2025-03-10 03:37:56 +0100

    Implement and test new code in BingoGame class

--- src/main/java/bingo/game/BingoGame.java
+++ src/main/java/bingo/game/BingoGame.java
@@ -1,5 +1,10 @@
 package bingo.game;
 
+import bingo.game.results.BingoResult;
+import bingo.game.results.BingoResultBar;
+import bingo.rules.RetryRule;
+import bingo.tokens.TokenCounter;
+
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
@@ -8,53 +13,131 @@ public class BingoGame {
     private static final int START_LEVEL = 1;
     private static final int MAX_LEVEL = 7;
 
-    private final List<ResultBar> resultBars;
+    private final List<BingoResultBar> resultBars;
+    private final TokenCounter tokenCounter;
+    private boolean challengeEndedVoluntarily;
     private Optional<BingoResult> bingoResult;
-    private boolean challengeEnded;
+    private List<RetryRule> activeRetryRules;
+    private BingoGameState bingoGameState;
     private int currentLevel;
 
     public BingoGame() {
         this.resultBars = new LinkedList<>();
+        this.tokenCounter = new TokenCounter();
+        this.challengeEndedVoluntarily = false;
         for (int level = START_LEVEL - 1; level <= MAX_LEVEL; level++) {
-            resultBars.add(new ResultBar(level));
+            resultBars.add(new BingoResultBar(level));
         }
+        this.bingoGameState = BingoGameState.LEVEL_INITIALIZED;
         this.currentLevel = START_LEVEL;
-        doResetForCurrentLevel();
-    }
-
-    private void resetChallengeEndedFlag() {
-        challengeEnded = false;
+        resetBingoResult();
     }
 
     private void resetBingoResult() {
         bingoResult = Optional.empty();
+        activeRetryRules = new LinkedList<>();
     }
 
-    public void doResetForCurrentLevel() {
-        resetChallengeEndedFlag();
-        resetBingoResult();
+    private boolean processBingoGameAction(BingoGameAction selectedAction) {
+        return switch (bingoGameState) {
+            case LEVEL_INITIALIZED -> processActionForLevelInitializedState(selectedAction);
+            case UNCONFIRMED_RESULT -> processActionForUnconfirmedResultState(selectedAction);
+            case UNCONFIRMED_VOLUNTARY_END -> {
+                processActionForUnconfirmedVoluntaryEndState(selectedAction);
+                yield true;
+            }
+            case CHALLENGE_ENDED -> false;
+        };
     }
 
-    public void submitBingoResult(BingoResult bingoResult) {
-        this.bingoResult = Optional.ofNullable(bingoResult);
-        resetChallengeEndedFlag();
+    private boolean processActionForLevelInitializedState(BingoGameAction selectedAction) {
+        return switch (selectedAction) {
+            case SUBMIT_RESULT -> {
+                bingoGameState = BingoGameState.UNCONFIRMED_RESULT;
+                yield true;
+            }
+            case CONFIRM_RESULT -> false;
+            case RESET_WITHOUT_CONFIRMING -> true;
+            case END_CHALLENGE_VOLUNTARILY -> {
+                bingoGameState = BingoGameState.UNCONFIRMED_VOLUNTARY_END;
+                yield true;
+            }
+        };
     }
 
-    public void goToNextLevel() {
-        if (playerCanGoToNextLevel()) {
-            doResetForCurrentLevel();
-            currentLevel++;
+    private boolean processActionForUnconfirmedResultState(BingoGameAction selectedAction) {
+        return switch (selectedAction) {
+            case SUBMIT_RESULT -> true;
+            case CONFIRM_RESULT -> {
+                if (requirementOfCurrentResultBarIsMet()) {
+                    if (hasNextLevel()) {
+                        bingoGameState = BingoGameState.LEVEL_INITIALIZED;
+                        resetBingoResult();
+                        currentLevel++;
+                    } else {
+                        bingoGameState = BingoGameState.CHALLENGE_ENDED;
+                    }
+                } else {
+                    if (retryingIsAllowed()) {
+                        bingoGameState = BingoGameState.LEVEL_INITIALIZED;
+                        resetBingoResult();
+                    } else {
+                        bingoGameState = BingoGameState.CHALLENGE_ENDED;
+                    }
+                }
+                tokenCounter.confirmMatchResult();
+                yield true;
+            }
+            case RESET_WITHOUT_CONFIRMING -> {
+                bingoGameState = BingoGameState.LEVEL_INITIALIZED;
+                yield true;
+            }
+            case END_CHALLENGE_VOLUNTARILY -> false;
+        };
+    }
+
+    private void processActionForUnconfirmedVoluntaryEndState(BingoGameAction selectedAction) {
+        switch (selectedAction) {
+            case SUBMIT_RESULT -> bingoGameState = BingoGameState.UNCONFIRMED_RESULT;
+            case CONFIRM_RESULT -> {
+                bingoGameState = BingoGameState.CHALLENGE_ENDED;
+                challengeEndedVoluntarily = true;
+            }
+            case RESET_WITHOUT_CONFIRMING -> bingoGameState = BingoGameState.LEVEL_INITIALIZED;
+            case END_CHALLENGE_VOLUNTARILY -> {
+            }
         }
     }
 
-    public void endChallenge() {
-        if (playerCanGoToNextLevel()) {
-            challengeEnded = true;
+    public boolean doResetForCurrentLevel() {
+        boolean stateChangeSuccessful = processBingoGameAction(BingoGameAction.RESET_WITHOUT_CONFIRMING);
+        if (stateChangeSuccessful) {
+            resetBingoResult();
+            tokenCounter.cancelMatchResult();
         }
+        return stateChangeSuccessful;
     }
 
-    public boolean playerCanGoToNextLevel() {
-        return requirementOfCurrentResultBarIsMet() && hasNextLevel();
+    public boolean submitBingoResult(BingoResult bingoResult, List<RetryRule> activeRetryRules) {
+        boolean stateChangeSuccessful = processBingoGameAction(BingoGameAction.SUBMIT_RESULT);
+        if (stateChangeSuccessful) {
+            this.bingoResult = Optional.ofNullable(bingoResult);
+            this.activeRetryRules = activeRetryRules;
+            tokenCounter.calculateMatchResult(requirementOfCurrentResultBarIsMet(), hasNextLevel(), activeRetryRules);
+        }
+        return stateChangeSuccessful;
+    }
+
+    public boolean confirmCurrentResult() {
+        return processBingoGameAction(BingoGameAction.CONFIRM_RESULT);
+    }
+
+    public boolean endChallenge() {
+        return processBingoGameAction(BingoGameAction.END_CHALLENGE_VOLUNTARILY);
+    }
+
+    private boolean retryingIsAllowed() {
+        return !activeRetryRules.isEmpty() || tokenCounter.hasExtraLife();
     }
 
     private boolean requirementOfCurrentResultBarIsMet() {
@@ -69,10 +152,6 @@ public class BingoGame {
         return "Requirement of level %s: %s points".formatted(level, getPointRequirementOfLevel(level));
     }
 
-    private String getNumberOfSubsAsStringForLevel(int level) {
-        return resultBars.get(level).getNumberOfSubsAsString();
-    }
-
     private boolean hasNextLevel() {
         return currentLevel < MAX_LEVEL;
     }
@@ -81,7 +160,7 @@ public class BingoGame {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("| Level | Points required | Number of subs as reward: 2^(Level) |\n");
         stringBuilder.append("|---|---:|---:|\n");
-        for (ResultBar resultBar : resultBars) {
+        for (BingoResultBar resultBar : resultBars) {
             stringBuilder.append("| %s | %s | 2^%s = %s |\n".formatted(
                     resultBar.level(),
                     resultBar.getPointRequirement(),
@@ -93,31 +172,78 @@ public class BingoGame {
 
     @Override
     public String toString() {
-        if (challengeEnded) {
-            return "Challenge ended voluntarily on level %s. Your reward: %s".formatted(
-                    currentLevel,
-                    getNumberOfSubsAsStringForLevel(
-                            currentLevel));
-        }
         StringBuilder stringBuilder = new StringBuilder();
-        bingoResult.ifPresent(result -> stringBuilder.append(result).append(". "));
-        stringBuilder.append(getPointRequirementOfLevelAsString(currentLevel));
-        if (requirementOfCurrentResultBarIsMet()) {
-            stringBuilder.append(
-                    ", which means your result meets the point requirement, and you unlocked the reward for the current level: ");
-            stringBuilder.append(getNumberOfSubsAsStringForLevel(currentLevel));
-            if (hasNextLevel()) {
-                stringBuilder.append(
-                        ". You can now choose to end the challenge and receive your reward, or continue to the next level. ");
-                stringBuilder.append(getPointRequirementOfLevelAsString(currentLevel + 1));
-            } else {
-                stringBuilder.append(". This is the highest reward you can get. Congratulations!");
+        if (bingoGameState.equals(BingoGameState.UNCONFIRMED_VOLUNTARY_END) || challengeEndedVoluntarily) {
+            appendTextForVoluntaryEndOfChallenge(stringBuilder);
+        } else {
+            bingoResult.ifPresent(result -> stringBuilder.append(result).append(". "));
+            stringBuilder.append(getPointRequirementOfLevelAsString(currentLevel));
+            if (requirementOfCurrentResultBarIsMet()) {
+                appendTextForSuccessfulMatch(stringBuilder);
+            } else if (bingoResult.isPresent()) {
+                appendTextForUnsuccessfulMatch(stringBuilder);
             }
-        } else if (bingoResult.isPresent()) {
-            stringBuilder.append(
-                    ", which means your result does not meet the point requirement, and the challenge is over. You lose any unlocked rewards. Your reward for participating: ");
-            stringBuilder.append(getNumberOfSubsAsStringForLevel(0));
         }
+        appendTextIfBingoGameIsInChallengeEndedState(stringBuilder);
         return stringBuilder.toString();
+    }
+
+    private void appendTextForVoluntaryEndOfChallenge(StringBuilder stringBuilder) {
+        BingoResultBar previousResultBar = resultBars.get(currentLevel - 1);
+        stringBuilder.append("Challenge ended voluntarily on level ")
+                .append(currentLevel)
+                .append(". Your reward from the previous level: ")
+                .append(previousResultBar.getNumberOfSubsAsString())
+                .append(".");
+        appendTextForConversionOfExtraLives(previousResultBar, stringBuilder);
+    }
+
+    private void appendTextForSuccessfulMatch(StringBuilder stringBuilder) {
+        BingoResultBar currentResultBar = resultBars.get(currentLevel);
+        stringBuilder.append(
+                ", which means your result meets the point requirement, and you unlocked the reward for the current level: ");
+        stringBuilder.append(currentResultBar.getNumberOfSubsAsString()).append(". ");
+        if (hasNextLevel()) {
+            stringBuilder.append(tokenCounter)
+                    .append(" You can choose to end the challenge and receive your reward, or continue to the next level. ");
+            stringBuilder.append(getPointRequirementOfLevelAsString(currentLevel + 1));
+        } else {
+            stringBuilder.append("This is the highest reward you can get. Congratulations!");
+            appendTextForConversionOfExtraLives(currentResultBar, stringBuilder);
+        }
+    }
+
+    private void appendTextForUnsuccessfulMatch(StringBuilder stringBuilder) {
+        stringBuilder.append(", which means your result does not meet the point requirement. ");
+        if (retryingIsAllowed()) {
+            stringBuilder.append("You are allowed to retry ");
+            if (activeRetryRules.contains(RetryRule.IMBALANCED_MATCHMAKING)) {
+                stringBuilder.append("due to ").append(RetryRule.IMBALANCED_MATCHMAKING.getDisplayText().toLowerCase());
+            } else if (activeRetryRules.contains(RetryRule.UNFAIR_DISADVANTAGE)) {
+                stringBuilder.append("due to an ").append(RetryRule.UNFAIR_DISADVANTAGE.getDisplayText().toLowerCase());
+            } else if (tokenCounter.hasExtraLife()) {
+                stringBuilder.append("because you have an extra life");
+            }
+            stringBuilder.append(". ").append(tokenCounter);
+        } else {
+            stringBuilder.append(
+                    "The challenge is over and you lose any unlocked rewards. Your reward for participating: ");
+            stringBuilder.append(resultBars.getFirst().getNumberOfSubsAsString());
+        }
+    }
+
+    private void appendTextForConversionOfExtraLives(BingoResultBar resultBar, StringBuilder stringBuilder) {
+        if (tokenCounter.hasExtraLife()) {
+            int totalReward = resultBar.getNumberOfSubsAsReward() + tokenCounter.getCurrentExtraLives() * 6;
+            stringBuilder.append(" Your unused extra lives are converted to 6 subs each, for a total of ")
+                    .append(totalReward)
+                    .append(" subs.");
+        }
+    }
+
+    private void appendTextIfBingoGameIsInChallengeEndedState(StringBuilder stringBuilder) {
+        if (bingoGameState.equals(BingoGameState.CHALLENGE_ENDED)) {
+            stringBuilder.append("\n\nEnd of challenge confirmed. Changes are no longer allowed.");
+        }
     }
 }
--- src/test/java/bingo/game/BingoGameTest.java
+++ src/test/java/bingo/game/BingoGameTest.java
@@ -1,52 +1,34 @@
 package bingo.game;
 
+import bingo.game.results.BingoResult;
 import bingo.ribbons.Ribbon;
+import bingo.rules.RetryRule;
 import bingo.ships.MainArmamentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BingoGameTest {
     private static final int START_LEVEL = 1;
     private static final int MAX_LEVEL = 7;
+    private static final String END_OF_CHALLENGE_CONFIRMED =
+            "\n\nEnd of challenge confirmed. Changes are no longer allowed.";
+    private static final String LEVEL_SEVEN_CONGRATULATIONS =
+            ". Requirement of level 7: 1400 points, which means your result meets the point requirement, and you unlocked the reward for the current level: 128 subs. This is the highest reward you can get. Congratulations! Your unused extra lives are converted to 6 subs each, for a total of 134 subs.";
+    public static final String LEVEL_ONE_GAME_OVER =
+            ". Requirement of level 1: 400 points, which means your result does not meet the point requirement. The challenge is over and you lose any unlocked rewards. Your reward for participating: 1 sub";
 
+    private List<RetryRule> activeRetryRules;
     private BingoGame bingoGame;
 
     @BeforeEach
     void setup() {
+        activeRetryRules = new LinkedList<>();
         bingoGame = new BingoGame();
-    }
-
-    @Test
-    void playerCanGoToNextLevelShouldReturnFalseWhenNoResultWasSubmitted() {
-        assertFalse(bingoGame.playerCanGoToNextLevel());
-    }
-
-    @Test
-    void playerCanGoToNextLevelShouldReturnFalseWhenSubmittedResultIsInsufficient() {
-        submitInsufficientBingoResultForLevelOne();
-        assertFalse(bingoGame.playerCanGoToNextLevel());
-    }
-
-    @Test
-    void playerCanGoToNextLevelShouldReturnTrueWhenSubmittedResultIsSufficient() {
-        submitSufficientBingoResultForLevelOne();
-        assertTrue(bingoGame.playerCanGoToNextLevel());
-    }
-
-    @Test
-    void playerCanGoToNextLevelShouldReturnFalseWhenMaxLevelIsReached() {
-        BingoResult bingoResult = new BingoResult(MainArmamentType.SMALL_OR_MEDIUM_CALIBER_GUNS);
-        bingoResult.addRibbonResult(Ribbon.DESTROYED, 12);
-        for (int level = START_LEVEL; level < MAX_LEVEL; level++) {
-            bingoGame.submitBingoResult(bingoResult);
-            assertTrue(bingoGame.playerCanGoToNextLevel());
-            bingoGame.goToNextLevel();
-        }
-        bingoGame.submitBingoResult(bingoResult);
-        assertFalse(bingoGame.playerCanGoToNextLevel());
-        bingoGame.goToNextLevel();
     }
 
     @Test
@@ -73,23 +55,28 @@ class BingoGameTest {
 
     @Test
     void toStringMethodShouldReturnGameOverWhenSubmittedResultIsInsufficient() {
-        BingoResult bingoResult = submitInsufficientBingoResultForLevelOne();
-        assertToStringMethodReturnsGameOverForLevelOne(bingoResult);
+        BingoResult bingoResult = createInsufficientBingoResultForLevelOne();
+        assertSubmitBingoResultIsSuccessful(bingoResult);
+        assertEquals(bingoResult + LEVEL_ONE_GAME_OVER, bingoGame.toString());
+        assertConfirmCurrentResultIsSuccessful();
+        assertEquals(bingoResult + LEVEL_ONE_GAME_OVER.concat(END_OF_CHALLENGE_CONFIRMED), bingoGame.toString());
     }
 
     @Test
     void toStringMethodShouldReturnLevelTwoNextWhenSubmittedResultIsSufficient() {
-        BingoResult bingoResult = submitSufficientBingoResultForLevelOne();
+        BingoResult bingoResult = createSufficientBingoResultForLevelOne();
+        assertSubmitBingoResultIsSuccessful(bingoResult);
         assertEquals(
                 bingoResult +
-                        ". Requirement of level 1: 400 points, which means your result meets the point requirement, and you unlocked the reward for the current level: 2 subs. You can now choose to end the challenge and receive your reward, or continue to the next level. Requirement of level 2: 600 points",
+                        ". Requirement of level 1: 400 points, which means your result meets the point requirement, and you unlocked the reward for the current level: 2 subs. You gain 1 token for a successful match as per rule 9a. You now have 1 token. You can choose to end the challenge and receive your reward, or continue to the next level. Requirement of level 2: 600 points",
                 bingoGame.toString());
     }
 
     @Test
     void toStringMethodShouldReturnSecondResultBarWhenNoResultWasSubmitted() {
-        submitSufficientBingoResultForLevelOne();
-        bingoGame.goToNextLevel();
+        BingoResult bingoResult = createSufficientBingoResultForLevelOne();
+        assertSubmitBingoResultIsSuccessful(bingoResult);
+        assertConfirmCurrentResultIsSuccessful();
         assertToStringMethodReturnsSecondResultBar();
     }
 
@@ -99,95 +86,156 @@ class BingoGameTest {
         bingoResult.addRibbonResult(Ribbon.MAIN_GUN_HIT, 137);
         bingoResult.addRibbonResult(Ribbon.SET_ON_FIRE, 12);
         bingoResult.addRibbonResult(Ribbon.DESTROYED, 3);
-        bingoGame.submitBingoResult(bingoResult);
-        bingoGame.goToNextLevel();
-        bingoGame.submitBingoResult(bingoResult);
+        assertSubmitBingoResultIsSuccessful(bingoResult);
+        assertConfirmCurrentResultIsSuccessful();
+        assertSubmitBingoResultIsSuccessful(bingoResult);
         assertEquals(
                 bingoResult +
-                        ". Requirement of level 2: 600 points, which means your result meets the point requirement, and you unlocked the reward for the current level: 4 subs. You can now choose to end the challenge and receive your reward, or continue to the next level. Requirement of level 3: 800 points",
+                        ". Requirement of level 2: 600 points, which means your result meets the point requirement, and you unlocked the reward for the current level: 4 subs. You gain 1 token for a successful match as per rule 9a. You now have 2 tokens. You can choose to end the challenge and receive your reward, or continue to the next level. Requirement of level 3: 800 points",
                 bingoGame.toString());
     }
 
     @Test
-    void toStringMethodShouldReturnCongratulationsForLevelEightWhenSubmittedResultIsSufficient() {
+    void toStringMethodShouldReturnCongratulationsForLevelSevenWhenSubmittedResultIsSufficient() {
         BingoResult bingoResult = new BingoResult(MainArmamentType.SMALL_OR_MEDIUM_CALIBER_GUNS);
         bingoResult.addRibbonResult(Ribbon.DESTROYED, 12);
-        for (int level = START_LEVEL; level <= MAX_LEVEL; level++) {
-            bingoGame.submitBingoResult(bingoResult);
-            bingoGame.goToNextLevel();
+        for (int level = START_LEVEL; level < MAX_LEVEL; level++) {
+            assertSubmitBingoResultIsSuccessful(bingoResult);
+            assertConfirmCurrentResultIsSuccessful();
         }
+        assertSubmitBingoResultIsSuccessful(bingoResult);
+        assertEquals(bingoResult + LEVEL_SEVEN_CONGRATULATIONS, bingoGame.toString());
+        assertConfirmCurrentResultIsSuccessful();
         assertEquals(
-                bingoResult +
-                        ". Requirement of level 7: 1400 points, which means your result meets the point requirement, and you unlocked the reward for the current level: 128 subs. This is the highest reward you can get. Congratulations!",
+                bingoResult + LEVEL_SEVEN_CONGRATULATIONS.concat(END_OF_CHALLENGE_CONFIRMED),
                 bingoGame.toString());
     }
 
     @Test
-    void endChallengeShouldNotAffectToStringMethodWhenSubmittedResultIsInsufficient() {
-        BingoResult bingoResult = submitInsufficientBingoResultForLevelOne();
-        bingoGame.endChallenge();
-        assertToStringMethodReturnsGameOverForLevelOne(bingoResult);
+    void endChallengeShouldNotBePossibleWhenAnyResultIsSubmitted() {
+        BingoResult bingoResult = createSufficientBingoResultForLevelOne();
+        assertSubmitBingoResultIsSuccessful(bingoResult);
+        assertEndChallengeIsNotSuccessful();
+        assertConfirmCurrentResultIsSuccessful();
+        assertEndChallengeIsSuccessful();
+        assertEquals(
+                "Challenge ended voluntarily on level 2. Your reward from the previous level: 2 subs.",
+                bingoGame.toString());
     }
 
     @Test
-    void endChallengeShouldAffectToStringMethodWhenSubmittedResultIsSufficient() {
-        submitSufficientBingoResultForLevelOne();
-        bingoGame.endChallenge();
-        assertEquals("Challenge ended voluntarily on level 1. Your reward: 2 subs", bingoGame.toString());
+    void endChallengeShouldBePossibleWhenNoResultWasSubmitted() {
+        assertEndChallengeIsSuccessful();
+        assertEquals(
+                "Challenge ended voluntarily on level 1. Your reward from the previous level: 1 sub.",
+                bingoGame.toString());
     }
 
     @Test
-    void submitBingoResultShouldResetChallengeEndedFlag() {
-        submitSufficientBingoResultForLevelOne();
-        bingoGame.endChallenge();
-        BingoResult bingoResult = submitInsufficientBingoResultForLevelOne();
-        assertToStringMethodReturnsGameOverForLevelOne(bingoResult);
+    void endChallengeTwiceShouldBePossibleButHaveNoAdditionalEffect() {
+        assertEndChallengeIsSuccessful();
+        endChallengeShouldBePossibleWhenNoResultWasSubmitted();
     }
 
     @Test
-    void goToNextLevelShouldResetChallengeEndedFlag() {
-        submitSufficientBingoResultForLevelOne();
-        bingoGame.endChallenge();
-        bingoGame.goToNextLevel();
+    void submitBingoResultShouldBePossibleIfEndOfChallengeIsNotConfirmed() {
+        assertEndChallengeIsSuccessful();
+        BingoResult bingoResult = createSufficientBingoResultForLevelOne();
+        assertSubmitBingoResultIsSuccessful(bingoResult);
+        assertConfirmCurrentResultIsSuccessful();
         assertToStringMethodReturnsSecondResultBar();
     }
 
     @Test
-    void doResetForCurrentLevelShouldResetChallengeEndedFlag() {
-        submitSufficientBingoResultForLevelOne();
-        bingoGame.endChallenge();
-        bingoGame.doResetForCurrentLevel();
+    void submitBingoResultShouldBePossibleIfPreviousResultIsNotConfirmed() {
+        BingoResult insufficientBingoResult = createInsufficientBingoResultForLevelOne();
+        assertSubmitBingoResultIsSuccessful(insufficientBingoResult);
+        BingoResult sufficientBingoResult = createSufficientBingoResultForLevelOne();
+        assertSubmitBingoResultIsSuccessful(sufficientBingoResult);
+    }
+
+    @Test
+    void confirmCurrentResultShouldNotBePossibleWhenThereIsNothingToConfirm() {
+        assertConfirmCurrentResultIsNotSuccessful();
+        assertToStringMethodReturnsFirstResultBar();
+    }
+
+    @Test
+    void doResetForCurrentLevelShouldBePossibleWhenNoResultWasSubmitted() {
+        assertResetCurrentLevelIsSuccessful();
+        assertToStringMethodReturnsFirstResultBar();
+    }
+
+    @Test
+    void doResetForCurrentLevelShouldCancelEndOfChallenge() {
+        assertEndChallengeIsSuccessful();
+        assertResetCurrentLevelIsSuccessful();
         assertToStringMethodReturnsFirstResultBar();
     }
 
     @Test
     void doResetForCurrentLevelShouldRemovePreviouslySubmittedResult() {
-        submitSufficientBingoResultForLevelOne();
-        bingoGame.doResetForCurrentLevel();
+        BingoResult bingoResult = createSufficientBingoResultForLevelOne();
+        assertSubmitBingoResultIsSuccessful(bingoResult);
+        assertResetCurrentLevelIsSuccessful();
         assertToStringMethodReturnsFirstResultBar();
     }
 
-    private BingoResult submitInsufficientBingoResultForLevelOne() {
+    @Test
+    void noOtherActionsShouldBeAllowedWhenEndOfChallengeIsAlreadyConfirmed() {
+        assertEndChallengeIsSuccessful();
+        assertConfirmCurrentResultIsSuccessful();
+        BingoResult bingoResult = createSufficientBingoResultForLevelOne();
+        assertSubmitBingoResultIsNotSuccessful(bingoResult);
+        assertConfirmCurrentResultIsNotSuccessful();
+        assertResetCurrentLevelIsNotSuccessful();
+        assertEndChallengeIsNotSuccessful();
+    }
+
+    private BingoResult createInsufficientBingoResultForLevelOne() {
         BingoResult bingoResult = new BingoResult(MainArmamentType.SMALL_OR_MEDIUM_CALIBER_GUNS);
         bingoResult.addRibbonResult(Ribbon.MAIN_GUN_HIT, 37);
         bingoResult.addRibbonResult(Ribbon.SET_ON_FIRE, 2);
-        bingoGame.submitBingoResult(bingoResult);
         return bingoResult;
     }
 
-    private BingoResult submitSufficientBingoResultForLevelOne() {
+    private BingoResult createSufficientBingoResultForLevelOne() {
         BingoResult bingoResult = new BingoResult(MainArmamentType.SMALL_OR_MEDIUM_CALIBER_GUNS);
         bingoResult.addRibbonResult(Ribbon.MAIN_GUN_HIT, 237);
         bingoResult.addRibbonResult(Ribbon.SET_ON_FIRE, 12);
-        bingoGame.submitBingoResult(bingoResult);
         return bingoResult;
     }
 
-    private void assertToStringMethodReturnsGameOverForLevelOne(BingoResult bingoResult) {
-        assertEquals(
-                bingoResult +
-                        ". Requirement of level 1: 400 points, which means your result does not meet the point requirement, and the challenge is over. You lose any unlocked rewards. Your reward for participating: 1 sub",
-                bingoGame.toString());
+    private void assertSubmitBingoResultIsSuccessful(BingoResult bingoResult) {
+        assertTrue(bingoGame.submitBingoResult(bingoResult, activeRetryRules));
+    }
+
+    private void assertSubmitBingoResultIsNotSuccessful(BingoResult bingoResult) {
+        assertFalse(bingoGame.submitBingoResult(bingoResult, activeRetryRules));
+    }
+
+    private void assertConfirmCurrentResultIsSuccessful() {
+        assertTrue(bingoGame.confirmCurrentResult());
+    }
+
+    private void assertConfirmCurrentResultIsNotSuccessful() {
+        assertFalse(bingoGame.confirmCurrentResult());
+    }
+
+    private void assertResetCurrentLevelIsSuccessful() {
+        assertTrue(bingoGame.doResetForCurrentLevel());
+    }
+
+    private void assertResetCurrentLevelIsNotSuccessful() {
+        assertFalse(bingoGame.doResetForCurrentLevel());
+    }
+
+    private void assertEndChallengeIsSuccessful() {
+        assertTrue(bingoGame.endChallenge());
+    }
+
+    private void assertEndChallengeIsNotSuccessful() {
+        assertFalse(bingoGame.endChallenge());
     }
 
     private void assertToStringMethodReturnsFirstResultBar() {
