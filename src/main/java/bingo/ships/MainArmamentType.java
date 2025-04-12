62e5d41f9d3143e428848bb2bb9bd86a386a79dc
jbuerkle <joel.buerkle@web.de>
2025-04-13 00:06:54 +0200

    Add more categories in MainArmamentType class

--- src/main/java/bingo/ships/MainArmamentType.java
+++ src/main/java/bingo/ships/MainArmamentType.java
@@ -1,8 +1,10 @@
 package bingo.ships;
 
 public enum MainArmamentType {
-    SMALL_OR_MEDIUM_CALIBER_GUNS("Gun caliber up to 304mm"),
+    SMALL_CALIBER_GUNS("Gun caliber up to 202mm"),
+    MEDIUM_CALIBER_GUNS("Gun caliber of 203mm+"),
     LARGE_CALIBER_GUNS("Gun caliber of 305mm+"),
+    EXTRA_LARGE_CALIBER_GUNS("Gun caliber of 406mm+"),
     TORPEDOES("Torpedoes"),
     AIRCRAFT("Aircraft");
 
--- src/test/java/bingo/ships/MainArmamentTypeTest.java
+++ src/test/java/bingo/ships/MainArmamentTypeTest.java
@@ -8,8 +8,10 @@ class MainArmamentTypeTest {
 
     @Test
     void getDisplayTextShouldReturnCorrectText() {
-        assertEquals("Gun caliber up to 304mm", MainArmamentType.SMALL_OR_MEDIUM_CALIBER_GUNS.getDisplayText());
+        assertEquals("Gun caliber up to 202mm", MainArmamentType.SMALL_CALIBER_GUNS.getDisplayText());
+        assertEquals("Gun caliber of 203mm+", MainArmamentType.MEDIUM_CALIBER_GUNS.getDisplayText());
         assertEquals("Gun caliber of 305mm+", MainArmamentType.LARGE_CALIBER_GUNS.getDisplayText());
+        assertEquals("Gun caliber of 406mm+", MainArmamentType.EXTRA_LARGE_CALIBER_GUNS.getDisplayText());
         assertEquals("Torpedoes", MainArmamentType.TORPEDOES.getDisplayText());
         assertEquals("Aircraft", MainArmamentType.AIRCRAFT.getDisplayText());
     }
