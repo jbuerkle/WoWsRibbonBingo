930720fd53b6116eb51d7750a9e7ff363877eab5
jbuerkle <joel.buerkle@web.de>
2025-05-03 21:57:35 +0200

    Fix warning in UserInputException class

--- src/main/java/bingo/application/gui/input/UserInputException.java
+++ src/main/java/bingo/application/gui/input/UserInputException.java
@@ -1,6 +1,10 @@
 package bingo.application.gui.input;
 
+import java.io.Serial;
+
 public class UserInputException extends Exception {
+    @Serial
+    private static final long serialVersionUID = -3891620162498187015L;
 
     public UserInputException(String message, Exception cause) {
         super(message, cause);
