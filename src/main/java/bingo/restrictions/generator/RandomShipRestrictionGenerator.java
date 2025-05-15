fa684b7cad44b42a393d246fb5897a2b69965df1
jbuerkle <joel.buerkle@web.de>
2025-05-15 02:40:51 +0200

    Implement and test RandomShipRestrictionGenerator

---
+++ src/main/java/bingo/restrictions/generator/RandomShipRestrictionGenerator.java
@@ -0,0 +1,80 @@
+package bingo.restrictions.generator;
+
+import bingo.restrictions.ShipRestriction;
+import bingo.restrictions.impl.BannedMainArmamentType;
+import bingo.restrictions.impl.ForcedMainArmamentType;
+import bingo.ships.MainArmamentType;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class RandomShipRestrictionGenerator {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 100;
+
+    private final Random random;
+
+    private Map<Integer, ShipRestriction> shipRestrictionsByNumber;
+
+    RandomShipRestrictionGenerator(Random random) {
+        this.random = random;
+    }
+
+    public RandomShipRestrictionGenerator() {
+        this(new Random());
+    }
+
+    /**
+     * @param number a number between 1 and 100.
+     * @return a random {@link ShipRestriction} for the chosen number.
+     */
+    public ShipRestriction getForNumber(int number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            String message = "The number %s is outside of the allowed range (between %s and %s)".formatted(
+                    number,
+                    MIN_NUMBER,
+                    MAX_NUMBER);
+            throw new IllegalArgumentException(message);
+        }
+        assignShipRestrictionsToAllNumbers();
+        return shipRestrictionsByNumber.get(number);
+    }
+
+    Map<Integer, ShipRestriction> getShipRestrictionsByNumber() {
+        return shipRestrictionsByNumber;
+    }
+
+    void assignShipRestrictionsToAllNumbers() {
+        shipRestrictionsByNumber = new HashMap<>();
+        int assignedSlots = 0;
+        while (assignedSlots < MAX_NUMBER / 2) {
+            int number = random.nextInt(MIN_NUMBER, MAX_NUMBER + 1);
+            if (numberIsUnassigned(number)) {
+                shipRestrictionsByNumber.put(number, new BannedMainArmamentType(getRandomMainArmamentType()));
+                assignedSlots++;
+            }
+        }
+        for (int number = MIN_NUMBER; number <= MAX_NUMBER; number++) {
+            if (numberIsUnassigned(number)) {
+                shipRestrictionsByNumber.put(number, new ForcedMainArmamentType(getRandomMainArmamentType()));
+            }
+        }
+    }
+
+    private boolean numberIsUnassigned(int number) {
+        return !shipRestrictionsByNumber.containsKey(number);
+    }
+
+    private MainArmamentType getRandomMainArmamentType() {
+        int number = random.nextInt(1, 5);
+        return switch (number) {
+            case 1 -> MainArmamentType.SMALL_CALIBER_GUNS;
+            case 2 -> MainArmamentType.MEDIUM_CALIBER_GUNS;
+            case 3 -> MainArmamentType.LARGE_CALIBER_GUNS;
+            case 4 -> MainArmamentType.EXTRA_LARGE_CALIBER_GUNS;
+            default -> throw new IllegalArgumentException("Unexpected value %s from random number generator".formatted(
+                    number));
+        };
+    }
+}
---
+++ src/test/java/bingo/restrictions/generator/RandomShipRestrictionGeneratorTest.java
@@ -0,0 +1,121 @@
+package bingo.restrictions.generator;
+
+import bingo.restrictions.ShipRestriction;
+import bingo.restrictions.impl.BannedMainArmamentType;
+import bingo.restrictions.impl.ForcedMainArmamentType;
+import bingo.ships.MainArmamentType;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class RandomShipRestrictionGeneratorTest {
+    @Mock
+    private Random mockedRandom;
+
+    @Test
+    void getShipRestrictionsByNumberShouldReturnFiftyFiftyDistributionOfShipRestrictionTypes() {
+        RandomShipRestrictionGenerator randomShipRestrictionGenerator = new RandomShipRestrictionGenerator();
+
+        randomShipRestrictionGenerator.assignShipRestrictionsToAllNumbers();
+        Map<Integer, ShipRestriction> shipRestrictionsByNumber =
+                randomShipRestrictionGenerator.getShipRestrictionsByNumber();
+
+        List<BannedMainArmamentType> bannedMainArmamentTypes = getBannedMainArmamentTypes(shipRestrictionsByNumber);
+        List<ForcedMainArmamentType> forcedMainArmamentTypes = getForcedMainArmamentTypes(shipRestrictionsByNumber);
+        assertEquals(100, shipRestrictionsByNumber.size());
+        assertEquals(50, bannedMainArmamentTypes.size());
+        assertEquals(50, forcedMainArmamentTypes.size());
+    }
+
+    @Test
+    void getShipRestrictionsByNumberShouldReturnFourDifferentMainArmamentTypes() {
+        RandomShipRestrictionGenerator predictableShipRestrictionGenerator = mockRandomNextIntWithPredictableValues();
+
+        predictableShipRestrictionGenerator.assignShipRestrictionsToAllNumbers();
+        Map<Integer, ShipRestriction> shipRestrictionsByNumber =
+                predictableShipRestrictionGenerator.getShipRestrictionsByNumber();
+
+        List<BannedMainArmamentType> bannedMainArmamentTypes = getBannedMainArmamentTypes(shipRestrictionsByNumber);
+        assertBannedMainArmamentTypeIs(MainArmamentType.SMALL_CALIBER_GUNS, bannedMainArmamentTypes.getFirst());
+        assertBannedMainArmamentTypeIs(MainArmamentType.MEDIUM_CALIBER_GUNS, bannedMainArmamentTypes.get(1));
+        assertBannedMainArmamentTypeIs(MainArmamentType.LARGE_CALIBER_GUNS, bannedMainArmamentTypes.get(2));
+        assertBannedMainArmamentTypeIs(MainArmamentType.EXTRA_LARGE_CALIBER_GUNS, bannedMainArmamentTypes.get(3));
+        List<ForcedMainArmamentType> forcedMainArmamentTypes = getForcedMainArmamentTypes(shipRestrictionsByNumber);
+        assertForcedMainArmamentTypeIs(MainArmamentType.SMALL_CALIBER_GUNS, forcedMainArmamentTypes.get(2));
+        assertForcedMainArmamentTypeIs(MainArmamentType.MEDIUM_CALIBER_GUNS, forcedMainArmamentTypes.get(3));
+        assertForcedMainArmamentTypeIs(MainArmamentType.LARGE_CALIBER_GUNS, forcedMainArmamentTypes.get(4));
+        assertForcedMainArmamentTypeIs(MainArmamentType.EXTRA_LARGE_CALIBER_GUNS, forcedMainArmamentTypes.get(5));
+    }
+
+    @Test
+    void getForNumberShouldReturnShipRestrictionsForMinAndMaxNumber() {
+        RandomShipRestrictionGenerator randomShipRestrictionGenerator = new RandomShipRestrictionGenerator();
+
+        assertNotNull(randomShipRestrictionGenerator.getForNumber(1));
+        assertNotNull(randomShipRestrictionGenerator.getForNumber(100));
+    }
+
+    @Test
+    void getForNumberZeroShouldThrowIllegalArgumentException() {
+        RandomShipRestrictionGenerator randomShipRestrictionGenerator = new RandomShipRestrictionGenerator();
+
+        IllegalArgumentException exception =
+                assertThrows(IllegalArgumentException.class, () -> randomShipRestrictionGenerator.getForNumber(0));
+        assertEquals("The number 0 is outside of the allowed range (between 1 and 100)", exception.getMessage());
+    }
+
+    @Test
+    void getForNumberOneHundredOneShouldThrowIllegalArgumentException() {
+        RandomShipRestrictionGenerator randomShipRestrictionGenerator = new RandomShipRestrictionGenerator();
+
+        IllegalArgumentException exception =
+                assertThrows(IllegalArgumentException.class, () -> randomShipRestrictionGenerator.getForNumber(101));
+        assertEquals("The number 101 is outside of the allowed range (between 1 and 100)", exception.getMessage());
+    }
+
+    private void assertBannedMainArmamentTypeIs(
+            MainArmamentType expectedMainArmamentType, BannedMainArmamentType bannedMainArmamentType) {
+        assertEquals(expectedMainArmamentType, bannedMainArmamentType.mainArmamentType());
+    }
+
+    private void assertForcedMainArmamentTypeIs(
+            MainArmamentType expectedMainArmamentType, ForcedMainArmamentType forcedMainArmamentType) {
+        assertEquals(expectedMainArmamentType, forcedMainArmamentType.mainArmamentType());
+    }
+
+    private List<BannedMainArmamentType> getBannedMainArmamentTypes(
+            Map<Integer, ShipRestriction> shipRestrictionsByNumber) {
+        return shipRestrictionsByNumber.values()
+                .stream()
+                .filter(shipRestriction -> shipRestriction instanceof BannedMainArmamentType)
+                .map(shipRestriction -> (BannedMainArmamentType) shipRestriction)
+                .toList();
+    }
+
+    private List<ForcedMainArmamentType> getForcedMainArmamentTypes(
+            Map<Integer, ShipRestriction> shipRestrictionsByNumber) {
+        return shipRestrictionsByNumber.values()
+                .stream()
+                .filter(shipRestriction -> shipRestriction instanceof ForcedMainArmamentType)
+                .map(shipRestriction -> (ForcedMainArmamentType) shipRestriction)
+                .toList();
+    }
+
+    private RandomShipRestrictionGenerator mockRandomNextIntWithPredictableValues() {
+        AtomicInteger counterForRange100 = new AtomicInteger(0);
+        AtomicInteger counterForRange4 = new AtomicInteger(0);
+        when(mockedRandom.nextInt(1, 101)).thenAnswer(_ -> (counterForRange100.getAndIncrement() % 100) + 1);
+        when(mockedRandom.nextInt(1, 5)).thenAnswer(_ -> (counterForRange4.getAndIncrement() % 4) + 1);
+        return new RandomShipRestrictionGenerator(mockedRandom);
+    }
+}
