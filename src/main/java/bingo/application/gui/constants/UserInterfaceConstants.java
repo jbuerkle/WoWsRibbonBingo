bffc1a1d85d25aee3dfccf55fbd6f323629823bb
jbuerkle <joel.buerkle@web.de>
2025-07-14 00:37:29 +0200

    Move some reusable UI code to separate classes

---
+++ src/main/java/bingo/application/gui/constants/UserInterfaceConstants.java
@@ -0,0 +1,10 @@
+package bingo.application.gui.constants;
+
+public class UserInterfaceConstants {
+    public static final String APPLICATION_TITLE = "World of Warships Ribbon Bingo";
+    public static final String AUTOSAVE_DIRECTORY = "autosave";
+    public static final String EMPTY_STRING = "";
+
+    private UserInterfaceConstants() {
+    }
+}
---
+++ src/main/java/bingo/application/gui/utility/UserInterfaceUtility.java
@@ -0,0 +1,34 @@
+package bingo.application.gui.utility;
+
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.InputEvent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+import java.net.URL;
+
+public class UserInterfaceUtility {
+
+    public void setStyleSheetsFor(Scene scene) {
+        URL resourceUrl = getClass().getResource("/stylesheets/dark-mode.css");
+        if (resourceUrl != null) {
+            scene.getStylesheets().add(resourceUrl.toExternalForm());
+        }
+    }
+
+    public void setEventHandlers(Button button, EventHandler<InputEvent> eventHandler) {
+        button.setOnMouseClicked(eventHandler);
+        button.setOnKeyPressed(onPressEnterOrSpacePerform(eventHandler));
+    }
+
+    private EventHandler<KeyEvent> onPressEnterOrSpacePerform(EventHandler<InputEvent> eventHandler) {
+        return event -> {
+            KeyCode keyCode = event.getCode();
+            if (keyCode.equals(KeyCode.ENTER) || keyCode.equals(KeyCode.SPACE)) {
+                eventHandler.handle(event);
+            }
+        };
+    }
+}
