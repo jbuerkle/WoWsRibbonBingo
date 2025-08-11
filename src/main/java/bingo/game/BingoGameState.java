864dffe573d1a4801429b1c33a407df2ebb6bb64
jbuerkle <joel.buerkle@web.de>
2025-08-11 03:14:17 +0200

    Redefine BingoGameState class and write tests

--- src/main/java/bingo/game/BingoGameState.java
+++ src/main/java/bingo/game/BingoGameState.java
@@ -4,11 +4,25 @@ import java.io.Serial;
 import java.io.Serializable;
 
 public enum BingoGameState implements Serializable {
-    LEVEL_INITIALIZED,
-    UNCONFIRMED_RESULT,
-    UNCONFIRMED_VOLUNTARY_END,
-    CHALLENGE_ENDED;
+    LEVEL_INITIALIZED(false),
+    PARTIAL_RESULT_SUBMITTED(false),
+    UNCONFIRMED_VOLUNTARY_END(false),
+    UNCONFIRMED_SUCCESSFUL_MATCH(false),
+    UNCONFIRMED_UNSUCCESSFUL_MATCH(false),
+    CHALLENGE_ENDED_VOLUNTARILY(true),
+    CHALLENGE_ENDED_SUCCESSFULLY(true),
+    CHALLENGE_ENDED_UNSUCCESSFULLY(true);
 
     @Serial
     private static final long serialVersionUID = -1793552819069605261L;
+
+    private final boolean isFinal;
+
+    BingoGameState(boolean isFinal) {
+        this.isFinal = isFinal;
+    }
+
+    public boolean isFinal() {
+        return isFinal;
+    }
 }
---
+++ src/test/java/bingo/game/BingoGameStateTest.java
@@ -0,0 +1,35 @@
+package bingo.game;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class BingoGameStateTest {
+
+    @Test
+    void isFinalShouldReturnFalse() {
+        List<BingoGameState> nonFinalStates = List.of(
+                BingoGameState.LEVEL_INITIALIZED,
+                BingoGameState.PARTIAL_RESULT_SUBMITTED,
+                BingoGameState.UNCONFIRMED_VOLUNTARY_END,
+                BingoGameState.UNCONFIRMED_SUCCESSFUL_MATCH,
+                BingoGameState.UNCONFIRMED_UNSUCCESSFUL_MATCH);
+        for (BingoGameState state : nonFinalStates) {
+            assertFalse(state.isFinal());
+        }
+    }
+
+    @Test
+    void isFinalShouldReturnTrue() {
+        List<BingoGameState> finalStates = List.of(
+                BingoGameState.CHALLENGE_ENDED_VOLUNTARILY,
+                BingoGameState.CHALLENGE_ENDED_SUCCESSFULLY,
+                BingoGameState.CHALLENGE_ENDED_UNSUCCESSFULLY);
+        for (BingoGameState state : finalStates) {
+            assertTrue(state.isFinal());
+        }
+    }
+}
